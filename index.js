cb575b5577a8d675daa638c4af4b664b09ef5271
alexgaaranes <aaaranes@up.edu.ph>
2025-02-20 14:14:34 +0800

    added index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+// Name:	Alexander Gabriel A. Aranes
+// Section:	CMSC 100 C-3L
+
+
+// Functions
+
+// Checks if the input passwords are valid
+function validatePassword(pw1, pw2){
+	if(pw1 != pw2) return false;
+	if(pw1.length != pw2.length) return false;
+
+	var hasNum = false;
+	var hasUpper = false;
+	var hasLower = false;
+
+	pw1.split('').forEach((c,i)=>{
+		if(isNaN(c) && (c === c.toUpperCase())) hasUpper = true;
+		if(isNaN(c) && (c === c.toLowerCase())) hasLower = true;
+		if(!isNaN(c)) hasNum = true;
+	});
+	return (hasNum && hasUpper && hasLower);
+}
+
+// Reverses a password string
+function reversePassword(pw){
+	let reversedPw = '';
+	for(let i=pw.length-1; i>=0; i--){
+		reversedPw += pw[i];
+	}
+	return reversedPw;
+}
+
+// Stores the passwords on an object
+function storePassword(username, pw1, pw2){
+	const userCred = {
+		name: username,
+		newpassword: pw1
+	}
+
+	if(validatePassword(pw1, pw2)) userCred.newpassword = reversePassword(pw1);
+	return userCred;
+}
+
+
+// Sample Runs
+console.log(storePassword("John", "passwordKoto", "passwordKoto"));	// no number
+console.log(storePassword("John", "PASS12345", "PASS12345"));		// no lowercase
+console.log(storePassword("John", "pass12345", "pass12345"));		// no uppercase
+console.log(storePassword("John", "Pass1234", "Pass1234"));		// valid
+console.log(storePassword("John", "Pass123", "Pass12345"));		// too short
+console.log(storePassword("Alex", "MyBestPassword101", "MyBestPassword101"));	// valid
